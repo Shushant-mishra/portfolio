411d2ae56d42318708f5075d6895a16a03dd19d8
Shushant-mishra <shushantmishra3070@gmail.com>
2024-12-22 00:59:03 +0530

    Added responsiveness

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ modeToggleSwitch.addEventListener("change", () => {
     body.classList.remove("dark");
   }
 });
+
+const mobileNavBtn = document.getElementById("mobile-nav-trigger");
+const mobileNav = document.querySelector(".mobile-nav");
+mobileNavBtn.addEventListener("change", () => {
+  if (mobileNavBtn.checked) {
+    mobileNav.classList.add("mobile-nav-active");
+  } else {
+    mobileNav.classList.remove("mobile-nav-active");
+  }
+});
