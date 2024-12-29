fc2225e3a7099086a6cedfe4b90f1291e7c92e68
Shushant-mishra <shushantmishra3070@gmail.com>
2024-12-29 23:35:49 +0530

    Updated navbar responsiveness and style

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ modeToggleSwitch.addEventListener("change", () => {
     body.classList.remove("dark");
   }
 });
-  
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      let activeLink = document.querySelector(`.nav a[href="#${id}"]`);
+      if (activeLink) activeLink.classList.add("active");
+    }
+  });
+};
+
 const mobileNavBtn = document.getElementById("mobile-nav-trigger");
 const mobileNav = document.querySelector(".mobile-nav");
 mobileNavBtn.addEventListener("change", () => {
